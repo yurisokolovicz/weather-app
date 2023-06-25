f79de14a16ae50589b531096b620183ddb274ac2
yurisokolovicz <yurisokolovicz@gmail.com>
2023-06-24 22:30:01 -0300

    Implementing functions for API fetching data

---
+++ script.js
@@ -0,0 +1,45 @@
+function getWeatherData(location) {
+    const apiKey = '05763633a9b64264b6c12601232506';
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${encodeURIComponent(location)}`;
+
+    const loadingElement = document.getElementById('loading');
+    loadingElement.style.display = 'block';
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherData = processWeatherData(data);
+            displayWeatherData(weatherData);
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        })
+        .finally(() => {
+            loadingElement.style.display = 'none';
+        });
+}
+
+function processWeatherData(data) {
+    const location = data.location.name;
+    const temperature = data.current.temp_c;
+    const condition = data.current.condition.text;
+
+    return { location, temperature, condition };
+}
+
+function displayWeatherData(weatherData) {
+    const weatherElement = document.getElementById('weatherData');
+    weatherElement.innerHTML = `
+    <h2>Weather in ${weatherData.location}</h2>
+    <p>Temperature: ${weatherData.temperature}°C</p>
+    <p>Condition: ${weatherData.condition}</p>
+  `;
+}
+
+const locationForm = document.getElementById('locationForm');
+locationForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const locationInput = document.getElementById('locationInput');
+    const location = locationInput.value.trim();
+    getWeatherData(location);
+});
